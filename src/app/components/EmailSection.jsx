a355502a54bf5ce8a55f838c6243d8c8860eb795
karbejha <mohamad.karbejha@gmail.com>
2024-06-02 17:11:44 +0300

    add Formpree to email section

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -4,46 +4,10 @@ import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
+import { useForm, ValidationError } from "@formspree/react";
 
 const EmailSection = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
-  const [error, setError] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError(""); // Reset error state
-
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
-
-    try {
-      const response = await fetch(endpoint, options);
-      const resData = await response.json();
-
-      if (response.ok) {
-        console.log("Message sent.");
-        setEmailSubmitted(true);
-      } else {
-        setError(resData.message || "Something went wrong, please try again.");
-      }
-    } catch (error) {
-      setError("An error occurred. Please try again later.");
-      console.error("Error sending email:", error);
-    }
-  };
+  const [state, handleSubmit] = useForm("xayrgoae");
 
   return (
     <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
@@ -63,7 +27,7 @@ const EmailSection = () => {
         </div>
       </div>
       <div>
-        {emailSubmitted ? (
+        {state.succeeded ? (
           <span className="text-green-500 text-sm mt-2">
             Email sent successfully!
           </span>
@@ -80,6 +44,11 @@ const EmailSection = () => {
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Enter your email"
+              />
+              <ValidationError 
+                prefix="Email" 
+                field="email"
+                errors={state.errors}
               />
             </div>
             <div className="mb-6">
@@ -106,13 +75,13 @@ const EmailSection = () => {
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
                 placeholder="Let's talk about..."
               />
+              <ValidationError 
+                prefix="Message" 
+                field="message"
+                errors={state.errors}
+              />
             </div>
-            {error && (
-              <span className="text-red-500 text-sm mb-2">
-                {error}
-              </span>
-            )}
-            <button type="submit" className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
+            <button type="submit" disabled={state.submitting} className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full">
               Send Message
             </button>
           </form>
