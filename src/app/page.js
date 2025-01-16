141273fd43bbe66c42b7de20d56dbe26129807a7
karbejha <mohamad.karbejha@gmail.com>
2025-01-16 15:54:55 +0300

    Update favicon path and modify site metadata for portfolio

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
         <title>Mohamad Karbejha | Fullstack Developer</title>
-        <link rel="icon" href="images/icon.ico" />
+        <link rel="icon" href="icon.ico" />
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
