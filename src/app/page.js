2423d8ba5e38ed5a4d7f8d0c3d6696dd57d97817
karbejha <mohamad.karbejha@gmail.com>
2025-01-16 15:59:25 +0300

    Update favicon file path in page component

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
         <title>Mohamad Karbejha | Fullstack Developer</title>
-        <link rel="icon" href="icon.ico" />
+        <link rel="icon" href="favicon.ico" />
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
